6309842158f89cc4482131f04c5d512122471a10
ryccoatika <ryccoatika.ra@gmail.com>
2021-01-17 00:12:04 +0800

    move main activity into ui.main package
    and defining, implementing a lot of stuff

---
+++ app/src/main/java/com/ryccoatika/cryptocurrencypricelisting/ui/main/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.ryccoatika.cryptocurrencypricelisting.ui.main
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.ryccoatika.cryptocurrencypricelisting.R
+import com.ryccoatika.cryptocurrencypricelisting.core.domain.model.Cryptocurrency
+import com.ryccoatika.cryptocurrencypricelisting.core.domain.usecase.CryptocurrencyUseCase
+import com.ryccoatika.cryptocurrencypricelisting.core.ui.CryptocurrencyAdapter
+import org.koin.android.ext.android.inject
+
+class MainActivity : AppCompatActivity(), MainContract.MainView {
+
+    private val cryptocurrencyInteractor: CryptocurrencyUseCase by inject()
+    private lateinit var mainPresenter: MainContract.IMainPresenter
+    private val adapter: CryptocurrencyAdapter by lazy { CryptocurrencyAdapter() }
+
+    // ui
+    private var swipeRefresh: SwipeRefreshLayout? = null
+    private var cryptocurrencyRv: RecyclerView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mainPresenter = MainPresenter(this, cryptocurrencyInteractor)
+
+        swipeRefresh = findViewById(R.id.swipe_refresh)
+        cryptocurrencyRv = findViewById(R.id.cryptocurrency_rv)
+
+        swipeRefresh?.setOnRefreshListener { mainPresenter.onRefresh() }
+
+        // setting up recyclerview
+        cryptocurrencyRv?.adapter = adapter
+        cryptocurrencyRv?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val visibleItemCount = recyclerView.layoutManager?.childCount ?: 0
+                val totalItemCount = recyclerView.layoutManager?.itemCount ?: 0
+                val firstVisibleItemPos = (recyclerView.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+
+                if (swipeRefresh?.isRefreshing == true) return
+
+                if (firstVisibleItemPos + visibleItemCount >= totalItemCount)
+                    mainPresenter.onLoadNext()
+            }
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mainPresenter.onRefresh()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mainPresenter.onDestroy()
+    }
+
+    override fun onRefresh() {
+        adapter.clearCryptocurrencies()
+    }
+
+    override fun onShowLoading() {
+        swipeRefresh?.isRefreshing = true
+    }
+
+    override fun onHideLoading() {
+        swipeRefresh?.isRefreshing = false
+    }
+
+    override fun onSuccess(results: List<Cryptocurrency>) {
+        adapter.addCryptocurrencies(results)
+        Log.d("190401", "onSuccess\ncount: ${results.count()}")
+    }
+
+    override fun onEmpty() {
+        Log.d("190401", "onEmpty")
+    }
+
+    override fun onError(message: String) {
+        Log.d("190401", "onError\nmessage: $message")
+    }
+}
