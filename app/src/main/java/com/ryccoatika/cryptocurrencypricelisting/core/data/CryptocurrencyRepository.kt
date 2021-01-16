0a4589669399a821ce4bc9d7044353962a2afc91
ryccoatika <ryccoatika.ra@gmail.com>
2021-01-16 23:56:54 +0800

    implementing cryptocurrency repository

---
+++ app/src/main/java/com/ryccoatika/cryptocurrencypricelisting/core/data/CryptocurrencyRepository.kt
@@ -0,0 +1,23 @@
+package com.ryccoatika.cryptocurrencypricelisting.core.data
+
+import com.ryccoatika.cryptocurrencypricelisting.core.data.mappers.toCryptocurrencyDomain
+import com.ryccoatika.cryptocurrencypricelisting.core.data.remote.ApiResponse
+import com.ryccoatika.cryptocurrencypricelisting.core.data.remote.CryptocurrencyDataSource
+import com.ryccoatika.cryptocurrencypricelisting.core.domain.model.Cryptocurrency
+import com.ryccoatika.cryptocurrencypricelisting.core.domain.repository.ICryptocurrencyRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class CryptocurrencyRepository(
+    private val cryptocurrencyDataSource: CryptocurrencyDataSource
+) : ICryptocurrencyRepository {
+    override fun getAllCoins(start: Int, limit: Int): Flow<Resource<List<Cryptocurrency>>> =
+        flow {
+            emit(Resource.InProgress)
+            when(val response = cryptocurrencyDataSource.getAllCoins(start, limit)) {
+                is ApiResponse.Success -> emit(Resource.Success(response.data.data.map { it.toCryptocurrencyDomain() }))
+                is ApiResponse.Error -> emit(Resource.Error(response.message))
+                ApiResponse.Empty -> emit(Resource.Empty)
+            }
+        }
+}
