a6821e3cddfd2b36d54ad38aeee6ac6b2907116d
ryccoatika <ryccoatika.ra@gmail.com>
2021-01-16 23:58:22 +0800

    create cryptocurrency interactor implementing cryptocurrency usecase

---
+++ app/src/main/java/com/ryccoatika/cryptocurrencypricelisting/core/domain/usecase/CryptocurrencyInteractor.kt
@@ -0,0 +1,13 @@
+package com.ryccoatika.cryptocurrencypricelisting.core.domain.usecase
+
+import com.ryccoatika.cryptocurrencypricelisting.core.data.Resource
+import com.ryccoatika.cryptocurrencypricelisting.core.domain.model.Cryptocurrency
+import com.ryccoatika.cryptocurrencypricelisting.core.domain.repository.ICryptocurrencyRepository
+import kotlinx.coroutines.flow.Flow
+
+class CryptocurrencyInteractor(
+    private val cryptocurrencyRepository: ICryptocurrencyRepository
+) : CryptocurrencyUseCase {
+    override fun getAllCoins(start: Int, limit: Int): Flow<Resource<List<Cryptocurrency>>> =
+        cryptocurrencyRepository.getAllCoins(start, limit)
+}
