87fa1ecb9ecb7360ee64eb418cf0f151d3f163b2
ryccoatika <ryccoatika.ra@gmail.com>
2021-01-18 01:59:45 +0700

    mapper changed

--- app/src/main/java/com/ryccoatika/cryptocurrencypricelisting/core/data/mappers/CryptocurrencyResponseMapper.kt
+++ app/src/main/java/com/ryccoatika/cryptocurrencypricelisting/core/data/mappers/CryptocurrencyResponseMapper.kt
@@ -5,6 +5,17 @@ import com.ryccoatika.cryptocurrencypricelisting.core.domain.model.Cryptocurrenc
 
 fun CryptocurrencyResponse?.toCryptocurrencyDomain(): Cryptocurrency =
     Cryptocurrency(
+        data = this?.data?.map { it.toCryptocurrencyDataDomain() } ?: emptyList(),
+        info = this?.info.toCryptocurrencyInfoDomain()
+    )
+
+fun CryptocurrencyResponse.InfoResponse?.toCryptocurrencyInfoDomain(): Cryptocurrency.Info =
+    Cryptocurrency.Info(
+        coinsNum = this?.coinsNum ?: 0
+    )
+
+fun CryptocurrencyResponse.CryptocurrencyDataResponse?.toCryptocurrencyDataDomain(): Cryptocurrency.CryptocurrencyData =
+    Cryptocurrency.CryptocurrencyData(
         id = this?.id ?: "",
         symbol = this?.symbol ?: "",
         name = this?.name ?: "",
