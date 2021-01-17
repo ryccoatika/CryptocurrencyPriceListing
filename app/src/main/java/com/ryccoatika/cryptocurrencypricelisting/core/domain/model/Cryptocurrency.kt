24f9b5c39a162b5c8d19e50101990c49f9e13159
ryccoatika <ryccoatika.ra@gmail.com>
2021-01-18 01:55:49 +0700

    add info into domain model to store total of cryptocurrency from api response

--- app/src/main/java/com/ryccoatika/cryptocurrencypricelisting/core/domain/model/Cryptocurrency.kt
+++ app/src/main/java/com/ryccoatika/cryptocurrencypricelisting/core/domain/model/Cryptocurrency.kt
@@ -4,26 +4,34 @@ import java.text.NumberFormat
 import java.util.*
 
 data class Cryptocurrency(
-    val id: String,
-    val symbol: String,
-    val name: String,
-    val nameId: String,
-    val rank: Int,
-    val priceUsd: String,
-    val percentChange24h: String,
-    val percentChange1h: String,
-    val percentChange7d: String,
-    val priceBtc: String,
-    val marketCapUsd: String,
-    val volume24: Double,
-    val volume24a: Double,
-    val cSupply: String,
-    val tSupply: String,
-    val mSupply: String
-)
+    val data: List<CryptocurrencyData>,
+    val info: Info
+) {
+    data class Info(
+        val coinsNum: Int
+    )
+    data class CryptocurrencyData(
+        val id: String,
+        val symbol: String,
+        val name: String,
+        val nameId: String,
+        val rank: Int,
+        val priceUsd: String,
+        val percentChange24h: String,
+        val percentChange1h: String,
+        val percentChange7d: String,
+        val priceBtc: String,
+        val marketCapUsd: String,
+        val volume24: Double,
+        val volume24a: Double,
+        val cSupply: String,
+        val tSupply: String,
+        val mSupply: String
+    )
+}
 
-val Cryptocurrency.imageUrl
+val Cryptocurrency.CryptocurrencyData.imageUrl
     get() = "https://www.coinlore.com/img/$nameId.png"
 
-val Cryptocurrency.formattedPriceUsd
+val Cryptocurrency.CryptocurrencyData.formattedPriceUsd
     get() = NumberFormat.getCurrencyInstance(Locale.US).format(priceUsd.toDoubleOrNull() ?: 0.0)
